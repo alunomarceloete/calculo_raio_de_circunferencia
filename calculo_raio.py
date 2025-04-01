4a3eefb152e873374ead571cf700960ea2ff69e5
Marcelo <alunomarcelo.ete@gmail.com>
2025-04-01 11:43:17 -0300

    Add files via upload

---
+++ calculo_raio.py
@@ -0,0 +1,9 @@
+
+#( CALCULO RAIO DE CIRCUNFERÊNCIA ), ALUNO: MARCELO 
+pi = 3.14
+raio = float(input('Digite o valor do raio: '))
+area = pi * raio**2
+print('Sua área é de: ', area, 'cm²')
+perimetro = 2 * pi * raio
+print('Seu perímetro é de: ', perimetro, 'cm')
+
